a1894eaa20cf985d614e01bce4658c46abc1438e
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-18 15:33:33 -0500

    created form with textboxes

---
+++ src/components/APIForm.jsx
@@ -0,0 +1,30 @@
+
+function APIForm({inputs, handleChange, onSubmit}) {
+    return (
+        <div>
+            <h2>Select Your Image Attributes:</h2>
+            <form className="form-container">
+                {inputs &&
+                    Object.entries(inputs).map(([category, value], index) => {
+                        <li className="form" key={index}>
+                            <h2>{category} </h2>
+                            <input
+                                type="text"
+                                name={category}
+                                value={value}
+                                placeholder="Input this attribute..."
+                                onChange={handleChange}
+                                className="textbox"
+                            />
+                            <br></br>
+                            <br></br>
+                            <p>{inputsInfo[index]}</p>
+                        </li>
+                    })}
+
+            </form>
+        </div>
+    )
+}
+
+export default APIForm;
