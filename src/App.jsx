dc58525caa7f22339ddf78a2f1a9e9ca14b12035
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-18 15:58:02 -0500

    added APIForm component to return

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import APIForm from './components/APIForm';
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
-const [inputs, setInputs] = useState({
-  url: "",
-  format: "",
-  no_ads: "",
-  no_cookie_banners: "",
-  width: "",
-  height: "",
-});
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {
+    
+  }
 
   return (
-    <div>
+    <div className='whole-page'>
+      <h1>Build Your Own Screenshot 📸</h1>
+
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) => 
+          setInputs((prevState) => ({
+            ...prevState, 
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+        />
+        <br></br>
 
     </div>
   )
