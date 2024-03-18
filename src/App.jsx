d63636e46517d4aa2b71f38135ea7576af587a35
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-18 15:20:57 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
