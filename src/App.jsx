761562ff26f9852a55453d1cc30f2a78dde5737a
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-18 19:20:06 -0500

    created helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentImage, setCurrentImage] = useState(null)
   const [inputs, setInputs] = useState({
     url: "",
     format: "",
@@ -34,6 +35,32 @@ function App() {
           inputs[key] = defaultValues[key]
         }
       }
+    }
+    makeQuery()
+  }
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+
+    callAPI(query).catch(console.error)
+  }
+
+  let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+
+
+  const callAPI = async (query) => {
+    const response = await fetch (query)
+    const json = await response.json()
+
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+        }
+    else {
+      setCurrentImage(json.url);
     }
   }
 
